4c6b88e9f24b83bdc3a08efff526d0896e14fc53
Your Name <you@example.com>
2025-09-05 19:39:35 +0800

    updated

--- partials/header.php
+++ partials/header.php
@@ -41,40 +41,6 @@
             </nav>
             <?php
         }
-        if (basename($_SERVER['PHP_SELF']) == 'osr6.php') {
-            ?>
-            <nav id="navmenu" class="navmenu">
-                <ul>
-                    <li><a href="#hero" class="active">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#team">Team</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
-            <?php
-        } if (basename($_SERVER['PHP_SELF']) == 'zoom-registration.php') {
-
-            ?>
-            <nav id="navmenu" class="navmenu">
-                <ul>
-                    <?php
-                    $links = [
-                        ['href' => 'osr6.php', 'label' => 'Home'],
-                    ];
-
-                    foreach ($links as $link) {
-                        $active = (basename($_SERVER['PHP_SELF']) == $link['href']) ? ' class="active"' : '';
-                        echo '<li><a href="' . $link['href'] . '"' . $active . '>' . $link['label'] . '</a></li>';
-                    }
-                    ?>
-                </ul>
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
-            <?php
-        }
         ?>
 
     </div>
